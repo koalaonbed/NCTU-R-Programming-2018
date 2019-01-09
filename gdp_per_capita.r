87aeea2f7f8a576fdc7973e690f47bb25cea3298
koalaonbed <34034076+koalaonbed@users.noreply.github.com>
2019-01-09 22:22:37 +0800

    plus GIF

--- gdp_per_capita.r
+++ gdp_per_capita.r
@@ -1,21 +1,22 @@
 library(rworldmap)
-library(RColorBrewer)
-gdp <- read.csv("gdpper.csv");
+gdpper <- read.csv("gdpper.csv");
+
+
 
 
 #colorRGB <- rep(rgb(1,1,1),248*57)
 #dim(colorRGB) <- c(266,61)
 #for(i in 5: 62){
 #	for(j in 1:266){
-#		gdp[i,j] <- gdp[i,j]/10000
+#		gdp[i,j]=gdp[i,j]*0.0001
 #	}
 #}
 
-mapped_data <- joinCountryData2Map(gdp, joinCode = "ISO3", nameJoinColumn = "Country.Code")
+mapped_data <- joinCountryData2Map(gdpper, joinCode = "ISO3", nameJoinColumn = "Country.Code")
 
 for (year in 1960:2017) {
   picname <- paste(year,'.png',sep='')
-  png(file = picname, width = 1080, height = 720)
+  png(file = picname, width = 1920, height = 1080)
   par(mai=c(0,0,0.2,0),xaxs="i",yaxs="i")
   k<-paste0("X",year)
   mapCountryData(mapped_data, nameColumnToPlot = k)
